a3bb7ce18f49ec36b85d5091d8542c6932f1db6a
Brandon <1brandonsolo@gmail.com>
2022-11-14 14:38:40 +0100

    Resultados calculados, aún no entregable

--- P1/looper.sh
+++ P1/looper.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for i in {1..16}
-do
-   python p1hpc.py "$i"
+for i in {2..16}; do
+  echo "$i cores"
+  for j in {0.1,0.25,0.5,0.75}; do
+    echo "$j proportion of data"
+    python p1hpc.py "$i" "$j"
+    done
 done
